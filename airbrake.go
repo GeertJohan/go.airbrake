79ef88b64548b8980be8ab15db918d8f9fd7e9e9
Geert-Johan Riemer <geertjohan.riemer@gmail.com>
2013-11-11 10:03:33 +0100

    Added setup for ServeMux

--- airbrake.go
+++ airbrake.go
@@ -297,6 +297,38 @@ func (b *Brake) WrapHTTPHandlerFunc(handler http.HandlerFunc) http.HandlerFunc {
 	}
 }
 
+// type ServeMux struct {
+// 	//++
+// }
+
+// func (b *Brake) NewServeMux() *ServeMux {
+// 	//++
+// }
+
+// // Handle registers the handler for the given pattern. If a handler already exists for pattern, Handle panics.
+// func (mux *ServeMux) Handle(pattern string, handler Handler) {
+// 	//++
+// }
+
+// // HandleFunc registers the handler function for the given pattern.
+// func (mux *ServeMux) HandleFunc(pattern string, handler func(ResponseWriter, *Request)) {
+// 	//++
+// }
+
+// // Handler returns the handler to use for the given request, consulting r.Method, r.Host, and r.URL.Path. It always returns a non-nil handler. If the path is not in its canonical form, the handler will be an internally-generated handler that redirects to the canonical path.
+// //
+// // Handler also returns the registered pattern that matches the request or, in the case of internally-generated redirects, the pattern that will match after following the redirect.
+// //
+// // If there is no registered handler that applies to the request, Handler returns a “page not found” handler and an empty pattern.
+// func (mux *ServeMux) Handler(r *Request) (h Handler, pattern string) {
+// 	//++
+// }
+
+// // ServeHTTP dispatches the request to the handler whose pattern most closely matches the request URL.
+// func (mux *ServeMux) ServeHTTP(w ResponseWriter, r *Request) {
+// 	//++
+// }
+
 // ErrorData sends an error with data to airbrake
 //
 // example:
