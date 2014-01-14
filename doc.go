37213126775e9dc973ccd8a487f274d2d32dd39c
Geert-Johan Riemer <geertjohan.riemer@gmail.com>
2014-01-14 14:45:09 +0100

    doc.go newlines

--- doc.go
+++ doc.go
@@ -1,4 +1,6 @@
-// go.airbrake provides methods to use the Airbrake error logging service
+// go.airbrake provides methods to use the Airbrake error logging service.
+//
 // For examples visit https://github.com/GeertJohan/go.airbrake
+//
 // For more information about Airbrake, visit: https://airbrake.io
 package airbrake
