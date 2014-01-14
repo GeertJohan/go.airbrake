355191206fd3d21db756d92df7a224237ee9a3ce
Geert-Johan Riemer <geertjohan.riemer@gmail.com>
2014-01-14 14:44:30 +0100

    Update doc.go; remove "DO NOT USE" message, add reference to airbrake.io and github.com

--- doc.go
+++ doc.go
@@ -1,3 +1,4 @@
-// Package airbrake should not be used just yet.
-// DO NOT USE THIS PACKAGE. EVERY ELEMENT IS SUBJECT TO CHANGE
+// go.airbrake provides methods to use the Airbrake error logging service
+// For examples visit https://github.com/GeertJohan/go.airbrake
+// For more information about Airbrake, visit: https://airbrake.io
 package airbrake
